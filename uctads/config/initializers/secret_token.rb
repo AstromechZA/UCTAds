8a5ae7b99ac7c81a07e5bbc862db2c7fa34fb022
Ben Meier <benmeier42@gmail.com>
2014-01-26 13:12:18 -0800

    init project and rb version

---
+++ uctads/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Uctads::Application.config.secret_key_base = 'd58635db1fbfd2a923d516434c5c88057267adcae7fa744199d39092d94a8f70edbfdfdd954db7dd70bd345d5d3df25b9b998bda210e852f8fc8183aebdb80da'
---
+++ uctads/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Uctads::Application.config.session_store :cookie_store, key: '_uctads_session'
