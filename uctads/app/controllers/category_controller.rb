d10478d69a522dac2c7de311aa26bec25f37c122
Ben Meier <benmeier42@gmail.com>
2014-01-30 08:14:06 -0800

    added tree view

--- uctads/app/controllers/category_controller.rb
+++ uctads/app/controllers/category_controller.rb
@@ -2,4 +2,8 @@ class CategoryController < ApplicationController
   def index
     @categories = Category.all
   end
+
+  def tree
+    @categories = Category.hash_tree
+  end
 end
--- uctads/app/helpers/category_helper.rb
+++ uctads/app/helpers/category_helper.rb
@@ -1,2 +1,10 @@
 module CategoryHelper
+
+
+  def nested_categories_view(categories)
+    categories.map do |category, sub_categories|
+      '<p>' + category.name + '</p><div style="margin-left:20px">' + nested_categories_view(sub_categories) + '</div>'
+    end.join.html_safe
+  end
+
 end
