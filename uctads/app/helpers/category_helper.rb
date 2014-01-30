ae3766c0748695f8b0293113a724d27491a88381
Ben Meier <benmeier42@gmail.com>
2014-01-30 08:55:20 -0800

    added fields to render

--- uctads/app/helpers/category_helper.rb
+++ uctads/app/helpers/category_helper.rb
@@ -4,7 +4,11 @@ module CategoryHelper
   # This is faster than using recursive partical views.
   def nested_categories_view(categories)
     ('<ul>' + categories.map do |category, sub_categories|
-      '<li><p>' + category.name + '</p>' + nested_categories_view(sub_categories) + '</li>'
+      o = '<li>'
+      o += '<p>' + category.name + ' : '
+      o += (category.fields.nil? ? '[]' : category.fields.keys.to_s) + '</p>'
+      o += nested_categories_view(sub_categories)
+      o + '</li>'
     end.join + '</ul>').html_safe
   end
 
