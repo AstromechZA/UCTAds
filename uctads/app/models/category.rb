dd49314d2a60630731fdd5a559f3d5ef92098360
Ben Meier <benmeier42@gmail.com>
2014-02-22 09:02:06 -0800

    added style to category#show

--- uctads/app/models/category.rb
+++ uctads/app/models/category.rb
@@ -21,6 +21,12 @@ class Category < ActiveRecord::Base
   end
 
   def cant_have_conflicting_fields
+    # duplicate field names
+    dups = fields.keys - fields.keys.uniq
+    if not dups.empty?
+      errors.add(:fields, "Duplicate field names! (#{dups.join(' ')})")
+    end
+
     # test ancestor fields
     conflicts = ancestors.map {|a| a.fields.keys}.flatten & fields.keys
     if not conflicts.empty?
@@ -34,6 +40,11 @@ class Category < ActiveRecord::Base
         errors.add(:fields, "Fields (#{conflicts.join(' ')}) conflict with descendant #{d.name}")
       end
     end
+  end
+
+  def cant_have_duplicate_selectables
+
+
   end
 
 end
