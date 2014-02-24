32c9023ffaefb9457f444de5aaff6dadd34cc948
Ben Meier <benmeier42@gmail.com>
2014-02-24 23:41:53 +0200

    working sort of

--- uctads/app/controllers/uploads_controller.rb
+++ uctads/app/controllers/uploads_controller.rb
@@ -4,26 +4,19 @@ class UploadsController < ApplicationController
     @uploads = Upload.all
   end
 
+  def show
+  end
+
   def new
     @upload = Upload.new
   end
 
   def create
-    @upload = Upload.new(upload_params)
-    if @upload.save
-      redirect_to @upload, notice: 'Upload uploaded'
-    else
-
-      render action: 'new'
-    end
-  end
-
-  def show
-    @upload = Upload.find(params[:id])
+    @upload = Upload.create(upload_params)
   end
 
   private
     def upload_params
-      params.require(:upload).permit(:image)
+      params[:upload].permit(:image)
     end
 end
