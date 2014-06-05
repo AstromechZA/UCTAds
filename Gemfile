9fcd295709ea1203cb3e72771912a139c9f774b7
Ben Meier <benmeier42@gmail.com>
2014-06-05 20:11:21 +0200

    moved files out of folder

--- Gemfile
+++ Gemfile
@@ -2,8 +2,6 @@ source 'https://rubygems.org'
 
 gem 'rails', '4.1.0'
 
-gem 'sqlite3'
-
 gem 'haml-rails'
 gem 'sass-rails', '~> 4.0.3'
 gem 'uglifier', '>= 1.3.0'
@@ -24,7 +22,12 @@ group :doc do
   gem 'sdoc', require: false
 end
 
+group :production do
+    gem 'pg'
+end
+
 group :development, :test do
+    gem 'sqlite3'
     gem 'faker'
     gem 'rspec-rails', '~> 3.0.0.beta'
     gem 'cucumber-rails', :require => false
--- config/application.rb
+++ config/application.rb
@@ -2,15 +2,10 @@ require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
 
-# Require the gems listed in Gemfile, including any gems
-# you've limited to :test, :development, or :production.
 Bundler.require(:default, Rails.env)
 
 module Uctads
   class Application < Rails::Application
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
 
     config.generators do |g|
       g.test_framework :rspec,
@@ -22,6 +17,8 @@ module Uctads
         :request_specs => true
       g.fixture_replacement :factory_girl, :dir => "spec/factories"
     end
+
+    config.time_zone = 'Pretoria'
 
     config.i18n.enforce_available_locales = true
   end
