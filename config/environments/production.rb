3ee8e04e9e159beed2ea0f77bfef8e0ab38965ab
Ben Meier <benmeier42@gmail.com>
2014-06-05 20:53:38 +0200

    added asset stuff

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -26,8 +26,8 @@ Uctads::Application.configure do
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
 
-  # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.serve_static_assets = true
+  config.assets.compile = true
 
   # Generate digests for assets URLs.
   config.assets.digest = true
