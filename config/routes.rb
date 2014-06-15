91b88aba04a322a32230e1e4710f4c119368d5f9
Ben Meier <benmeier42@gmail.com>
2014-06-16 00:29:00 +0200

    spaing

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Uctads::Application.routes.draw do
 
   # == categories routing ==
-  get '/categories', to: 'categories#index', as: 'categories'
-  get '/categories/new', to: 'categories#new', as: 'new_category'
-  post '/categories', to: 'categories#create', as: 'create_category'
-  get '/categories/:id', to: 'categories#show', as: 'show_category'
-  get '/categories/:id/edit', to: 'categories#edit', as: 'edit_category'
-  patch '/categories/:id', to: 'categories#update', as: 'update_category'
-  delete '/categories/:id', to: 'categories#destroy', as: 'destroy_category'
+  get '/categories',          to: 'categories#index',   as: 'categories'
+  get '/categories/new',      to: 'categories#new',     as: 'new_category'
+  post '/categories',         to: 'categories#create',  as: 'create_category'
+  get '/categories/:id',      to: 'categories#show',    as: 'show_category'
+  get '/categories/:id/edit', to: 'categories#edit',    as: 'edit_category'
+  patch '/categories/:id',    to: 'categories#update',  as: 'update_category'
+  delete '/categories/:id',   to: 'categories#destroy', as: 'destroy_category'
 
   get '/categories/:id/ancestor_fields', to: 'categories#ancestor_fields'
 
